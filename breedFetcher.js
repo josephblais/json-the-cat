c7c81bc7013b8a7a0e1f72930b6d079d394efb74
Joseph Blais <josephpierreblais@gmail.com>
2021-02-11 15:34:22 -0500

    refactored and wrote test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require('request');
-const input = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, res, body) => {
-  if (err) {
-    console.log(`Error: ${err}`);
-    return;
-  }
-  if (res.statusCode !== 200) {
-    console.log(`Error - status code: ${res.statusCode}`);
-    return;
-  } else {
-    const data = JSON.parse(body);
-    // console.log(data);
-    if (data === undefined || data.length === 0) {
-      console.log(`Error: cat not found`);
-      return;
-    } else {
-      console.log(data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
+    if (err) {
+      return callback(err, null);
     }
-  }
-});
+    if (res.statusCode !== 200) {
+      return (`Error - status code: ${res.statusCode}`);
+    } else {
+      const data = JSON.parse(body);
+      // console.log(data);
+      if (data === undefined || data.length === 0) {
+        return callback(`cat not found`, null);
+      } else {
+        return callback(null, data[0].description);
+      }
+    }
+  });
+
+};
+
+
+
+module.exports = { fetchBreedDescription };
